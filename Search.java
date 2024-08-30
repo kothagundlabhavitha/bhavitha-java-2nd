8bbde22f83542b823e8e7dda4fdc56574933bda3
kothagundlabhavitha <161490665+kothagundlabhavitha@users.noreply.github.com>
2024-08-30 16:28:12 +0530

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,19 @@
+package com.exam;
+
+import java.util.Scanner;
+
+public class Search {
+	public static void main(String[] args) {
+		
+		  Scanner sc=new Scanner(System.in);
+		  int a=sc.nextInt();
+		  
+		  int b[]= {1,2,3,4,5};
+		  for(int c=0;c<b.length;c++)
+		  if(a==b[c]) {
+		   
+		   System.out.println("element is found");
+		   break;
+		  }
+	}
+}
